eb366d203c7d2cd2db60539e4e35a6b7cf2edd61
Akshay Arora <akshaya@mindfiresolutions.com>
2016-11-09 18:04:14 +0530

    Added header and course page

---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,11 @@
+import React, { PropTypes } from 'react';
+
+class CoursesPage extends React.Component {
+  render () {
+    return(
+      <h1>This is where the courses go</h1>
+    );
+  }
+}
+
+export default CoursesPage;
